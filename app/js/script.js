2e19d41022d5e131dd9fc97467a566a4633ee3b9
nnadivictory25 <chikamagozi25@gmail.com>
2022-06-25 23:03:21 +0100

    fixed some bugs

--- app/js/script.js
+++ app/js/script.js
@@ -7,14 +7,15 @@ function calc() {
     const percentage = document.querySelector('#royalties').value/100
     const royalties = saleAmount * percentage
     const output = document.querySelector('#output')
+    const total = saleAmount - royalties
     const gain = saleAmount - royalties - buyAmount
 
     resetBtn.style.display = 'block'
 
-    if (buyAmount > gain) {
-        output.innerHTML = `The buying amount is ${buyAmount} sol, the selling amount is ${saleAmount} sol, the royalties is ${royalties} sol, the total amount credited to your wallet will be ${saleAmount - royalties} sol and your loss is ${gain} sol ☹️`
+    if (total + gain < buyAmount) {
+        output.innerHTML = `The buying amount is ${buyAmount} sol, the selling amount is ${saleAmount} sol, the royalties is ${royalties} sol, the total amount credited to your wallet will be ${total} sol and your loss is ${gain} sol ☹️`
     } else {
-        output.innerHTML = `The buying amount is ${buyAmount} sol, the selling amount is ${saleAmount} sol, the royalties is ${royalties} sol, the total amount credited to your wallet will be ${saleAmount - royalties} sol and your gain is ${gain} sol 😊 `
+        output.innerHTML = `The buying amount is ${buyAmount} sol, the selling amount is ${saleAmount} sol, the royalties is ${royalties} sol, the total amount credited to your wallet will be ${total} sol and your gain is ${gain} sol 😊 `
     }
 
     // console.log(`the selling amount was ${saleAmount} sol, the royalties was ${royalties} sol, the total amount credited to your wallet will be ${saleAmount - royalties} sol.`);
